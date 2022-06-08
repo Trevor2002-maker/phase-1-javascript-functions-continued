ded696215d9518b3876938a59283f232a305e3a9
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-08 22:31:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // code your solution here
-function saturdayFun(activity="roller-skate"){
-    return `This Saturday, I want to ${activity}`;
+function saturdayFun(activity="roller-skate!"){
+    return `This Saturday, I want to ${activity}!`;
 }
-const mondayWork= function(activity="go to the office"){
-    return `This Monday, I will ${activity}`;
+const mondayWork= function(activity="go to the office."){
+    return `This Monday, I will ${activity}.`;
 }
 
 const wrapAdjective = function(cost="*"){
